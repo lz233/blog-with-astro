1b614becb8f75b71190fbe52880dfb3e48d32ace
lz差不多是条咸鱼了 <lz23333@outlook.com>
2024-01-10 15:52:18 +0800

    Test Preview

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,7 +8,11 @@ const postsCollection = defineCollection({
     title: z.string(),
     pubDate: z.coerce.date(),
     description: z.string().optional(),
-    banner: image().optional(),
+    banner: image()
+      .refine((img) => Math.max(img.width, img.height) <= 4096, {
+        message: "Width and height of the banner must less than 4096 pixels"
+      })
+      .optional(),
     categories: z.array(z.string()),
     author: z.string().optional(),
     commentsUrl: z.string().optional(),
