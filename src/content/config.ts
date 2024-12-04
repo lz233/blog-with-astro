c4f4f1156373d07abab0ecf31586e3027eadae93
lz差不多是条咸鱼了 <lz23333@outlook.com>
2024-12-04 15:39:40 +0800

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
 
 import { defineCollection, z } from "astro:content";
 import { rssSchema } from './rssSchema';
+import { format } from "path";
 
 const posts = defineCollection({
   type: 'content',
@@ -9,11 +10,7 @@ const posts = defineCollection({
     description: z.string().optional(),
     pubDate: z.coerce.date(),
     customData: z.string().optional(),
-    banner: image()
-      .refine((img) => Math.max(img.width, img.height) <= 4096, {
-        message: "Width and height of the banner must less than 4096 pixels"
-      })
-      .optional(),
+    banner: image().optional(),
     categories: z.array(z.string()),
     author: z.string().optional(),
     commentsUrl: z.string().optional(),
